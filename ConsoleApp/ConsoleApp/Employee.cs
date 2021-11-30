39446cecc2aec6e8259c8d969dacb2658455ba69
RabiaImran <53070940+RabiaImran@users.noreply.github.com>
2021-11-30 14:03:24 +0500

    Add files via upload

---
+++ ConsoleApp/ConsoleApp/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public interface Employee
+    {
+        int EmployeeID { get; set; }
+        String EmployeeName { get; set; }
+        string EmployeeType { get; set; }
+        int NoHoursWork { get; set; }
+        int NoOfUnit { get; set; }
+        Hourly_Employee Hourly_Employee { get; set; }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Hourly_Employee.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Hourly_Employee : Employee
+    {
+        public int EmployeeID
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string EmployeeName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string EmployeeType
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int NoHoursWork
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int NoOfUnit
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        Hourly_Employee Employee.Hourly_Employee
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void CalculateSalary()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
